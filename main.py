9931e8c32c3a820f511dbea337555da76f50d79b
Cagan Boyraz <110572809+DoraTheTrader@users.noreply.github.com>
2023-01-03 00:44:43 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,33 @@
+import requests
+
+
+api_key = '87e177c8e539d4d1e2fe86f172233096'
+
+company = ['RJF', 'WY', 'AVB']
+
+years = 5
+
+for ticker in company:
+
+    income_statement = requests.get(f'https://financialmodelingprep.com/api/v3/income-statement/{ticker}?limit={years}&datatype=csv&apikey={api_key}')
+
+    with open(f'{ticker}_income_statement.csv', 'wb') as f:
+        f.write(income_statement.content)
+
+    balance_sheet = requests.get(f'https://financialmodelingprep.com/api/v3/balance-sheet-statement/{ticker}?limit={years}&datatype=csv&apikey={api_key}')
+
+    with open(f'{ticker}_balance_sheet.csv', 'wb') as f:
+        f.write(balance_sheet.content)
+
+    cash_flow = requests.get(f'https://financialmodelingprep.com/api/v3/cash-flow-statement/{ticker}?limit={years}&datatype=csv&apikey={api_key}')
+
+    with open(f'{ticker}_cash_flow.csv', 'wb') as f:
+        f.write(cash_flow.content)
+
+    enterprise_value = requests.get(f'https://financialmodelingprep.com/api/v3/enterprise-value/{ticker}?limit={years}&datatype=csv&apikey={api_key}')
+
+    with open(f'{ticker}_enterprise_value.csv', 'wb') as f:
+        f.write(cash_flow.content)
+
+
+
